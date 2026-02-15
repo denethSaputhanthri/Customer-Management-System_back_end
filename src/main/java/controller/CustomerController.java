4faa4f560d6716b9fe42e85b5cfc358812534626
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-15 16:09:03 +0530

    feat: create service layer and controller layer

---
+++ src/main/java/controller/CustomerController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+@Tag(name = "Customer-Controller",description = "Customer API Collection...")
+public class CustomerController {
+}
--- src/main/java/service/Impl/CustomerServiceImpl.java
+++ src/main/java/service/Impl/CustomerServiceImpl.java
@@ -1,4 +1,36 @@
 package service.Impl;
 
-public class CustomerServiceImpl {
+import model.dto.CustomerDto;
+import org.springframework.stereotype.Service;
+import service.CustomerService;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public Boolean addCustomer(CustomerDto customer) {
+        return null;
+    }
+
+    @Override
+    public Boolean updateCustomer(CustomerDto customer) {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteCustomer(Integer customerId) {
+        return null;
+    }
+
+    @Override
+    public CustomerDto searchById(Integer customerId) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDto> getAll() {
+        return List.of();
+    }
 }
