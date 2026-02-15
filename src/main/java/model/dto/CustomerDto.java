c50b46364cc401566eb77949d0f97a14a6e1499f
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-15 15:45:15 +0530

    feat: create dto and entity class

--- src/main/java/model/dto/CustomerDto.java
+++ src/main/java/model/dto/CustomerDto.java
@@ -1,4 +1,20 @@
 package model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
 public class CustomerDto {
+    private Integer customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
 }
--- src/main/java/model/entity/CustomerEntity.java
+++ src/main/java/model/entity/CustomerEntity.java
@@ -1,4 +1,24 @@
 package model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "customer")
 public class CustomerEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
 }
