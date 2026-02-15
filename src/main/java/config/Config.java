f73da4d2ce10a8d986fb624fa7fbae688d09af1d
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-15 16:12:44 +0530

    feat: create ModelMapper Config

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -1,4 +1,11 @@
 package config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
 public class Config {
+    @Bean
+    public ModelMapper getMapper(){
+        return new ModelMapper();
+    }
 }
